3f903e4594e50d6525bd3cd59e0344b45e32eeb8
Salih korkmaz <salihkorkmaz108478@gmail.com>
2024-07-20 17:50:21 +0300

    f_1

--- src/main/java/ders_video/requestparam/controller/MyController.java
+++ src/main/java/ders_video/requestparam/controller/MyController.java
@@ -1,7 +1,16 @@
 package ders_video.requestparam.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class MyController {
-    public String getMyMessage(String message){
-        return "";
+
+    @GetMapping("/message")
+    public String getMyMessage(@RequestParam("m") String message){
+        return "Your message is: " + message;
     }
 }
