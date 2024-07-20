c4840c74c277f7cfb12d25c4b11a14c6a5337cd8
Salih korkmaz <salihkorkmaz108478@gmail.com>
2024-07-20 17:41:09 +0300

    f_1

---
+++ src/main/java/ders_video/requestparam/controller/MyController.java
@@ -0,0 +1,7 @@
+package ders_video.requestparam.controller;
+
+public class MyController {
+    public String getMyMessage(String message){
+        return "";
+    }
+}
---
+++ src/test/java/ders_video/requestparam/RequestParamApplicationTests.java
@@ -0,0 +1,13 @@
+package ders_video.requestparam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestParamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
